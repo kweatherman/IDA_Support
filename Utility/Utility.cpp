5bbbd1da573cce4546a608717df978831bafd2e2
kweatherman <kevinweatherman@hotmail.com>
2025-05-11 01:39:20 -0400

    Added '#pragma warning(disable:4018)' to ignore an issue added in SDK 9.1

--- Utility/Utility.cpp
+++ Utility/Utility.cpp
@@ -21,6 +21,7 @@
 #pragma warning(push)
 #pragma warning(disable:4244) // conversion from 'ssize_t' to 'int', possible loss of data
 #pragma warning(disable:4267) // conversion from 'size_t' to 'uint32', possible loss of data
+#pragma warning(disable:4018) // warning C4018: '<': signed/unsigned mismatch
 #include <ida.hpp>
 #include <typeinf.hpp>
 #pragma warning(pop)
